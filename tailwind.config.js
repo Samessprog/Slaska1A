aa820cbf87c52c76a69f05c3bc039550eb70d28c
Samessprog <100785560+Samessprog@users.noreply.github.com>
2024-10-23 19:03:05 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Upewnij się, że dodane są wszystkie rozszerzenia
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
