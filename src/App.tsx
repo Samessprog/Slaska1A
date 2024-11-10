8cb5a37104be6887f715657d5293482fb147db6b
Samessprog <100785560+Samessprog@users.noreply.github.com>
2024-11-10 21:07:00 +0100

    Main Arrow page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./GeneralComponents/Navbar";
 import Footer from "./GeneralComponents/Footer";
 import Main from "./Pages/Mian";
+import { Link } from "react-router-dom";
 
 function App() {
   let [currentWindowWith, setCurrentWindowWith] = useState<number>(
@@ -21,10 +22,168 @@ function App() {
     };
   }, []);
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 1) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <Navbar currentWindowWith={currentWindowWith} />
       <Main />
+      <div className="flex flex-col">
+        {" "}
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+        Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+      </div>
+      {isVisible && (
+        <Link
+          to="#"
+          onClick={scrollToTop}
+          className={`arrow-up-page mr-5  ${isVisible ? "visible" : "none"}`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="50px"
+            viewBox="0 -960 960 960"
+            width="50px"
+            fill="undefined"
+            className="arrow-icon"
+          >
+            <path d="M480-528 296-344l-56-56 240-240 240 240-56 56-184-184Z" />
+          </svg>
+        </Link>
+      )}
+
       <Footer />
     </>
   );
