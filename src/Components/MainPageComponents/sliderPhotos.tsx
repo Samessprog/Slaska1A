547d6e6e6d79cb4af6a624984a6475e2b4a099fc
Samessprog <100785560+Samessprog@users.noreply.github.com>
2024-11-11 20:50:21 +0100

    a

--- src/Components/MainPageComponents/sliderPhotos.tsx
+++ src/Components/MainPageComponents/sliderPhotos.tsx
@@ -4,17 +4,14 @@ interface SliderPhotosInterface {
   elm: string;
 }
 
-
 const SliderPhotos: React.FC<SliderPhotosInterface> = ({ elm }) => {
   return (
     <div className="ml-3 mr-3">
       <img
         src={elm}
         alt="loading img err"
- MainPage
         className="w-full h-full object-cove t"
         className="w-full h-full object-cover"
- master
       />
     </div>
   );
