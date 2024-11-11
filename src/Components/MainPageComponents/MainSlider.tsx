6d5071698d2ed85cc94ac31fc16f8595e7abc9dd
Sames <100785560+Samessprog@users.noreply.github.com>
2024-11-11 20:32:37 +0100

    Main Slider (#9)

---
+++ src/Components/MainPageComponents/MainSlider.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import SliderPhotos from "./sliderPhotos";
+
+//imports test
+import p1slideTest from "../../assets/mbank-slide — kopia.jpg";
+import p1slideTest2 from "../../assets/mbank-slide.jpg";
+
+interface SliderSettings {
+  dots: boolean;
+  infinite: boolean;
+  speed: number;
+  slidesToShow: number;
+  slidesToScroll: number;
+  autoplay: boolean;
+  autoplaySpeed: number;
+}
+
+const MainSlider: React.FC = () => {
+  const photosArr = [p1slideTest, p1slideTest2];
+
+  const settings: SliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5500,
+  };
+
+  return (
+    <section className="flex justify-center w-full mb-10">
+      <div className=" justify-center w-full">
+        <div className="slider-container">
+          <Slider {...settings}>
+            {photosArr.map((elm, key) => (
+              <SliderPhotos elm={elm} key={key} />
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MainSlider;
