090d15cdc4e6e31d847b3b9be4a168eda5fc9fad
Samessprog <100785560+Samessprog@users.noreply.github.com>
2024-11-15 16:57:50 +0100

    AboutUS

---
+++ src/Components/MainPageComponents/AboutUs.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import t from "../../assets/mbank-slide — kopia.jpg";
+
+const AboutUS = () => {
+  return (
+    <section className="w-full flex pl-5 pr-5 mt-20">
+      <div className="w-6/12">
+        <img src={t} alt="loading err" />
+      </div>
+      <div className="w-6/12 text-wrap text-justify pr-10">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lobortis
+        magna at velit porta, vitae tempor enim interdum. Cras dignissim
+        accumsan turpis, in sodales enim ultricies eu. Ut eget rutrum nunc. Nam
+        ante elit, tincidunt viverra quam rhoncus, fermentum volutpat eros.
+        Pellentesque eget dolor eget lorem mattis tempus sed nec felis. Duis a
+        placerat magna. Proin mollis at eros sit amet semper. Sed hendrerit
+        lacus est. Aenean dui libero, consequat a quam sed, consequat
+        sollicitudin justo. Vestibulum eros justo, porttitor id eros ut, euismod
+        aliquet tortor. Suspendisse ut tincidunt enim. Donec mollis cursus
+        scelerisque. Proin euismod imperdiet elementum. Ut pulvinar magna quis
+        sollicitudin justo. Vestibulum eros justo, porttitor id eros ut, euismod
+        aliquet tortor. Suspendisse ut tincidunt enim. Donec mollis cursus
+        scelerisque. Proin euismod imperdiet elementum. Ut pulvinar magna quis
+      </div>
+    </section>
+  );
+};
+
+export default AboutUS;
