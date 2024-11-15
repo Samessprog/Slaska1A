5b46bc065194cdf34736c1be4497dd5fefb2204a
Sames <100785560+Samessprog@users.noreply.github.com>
2024-11-15 15:52:20 +0100

    Main page (#12)
    * Main Slider
    
    * Slider main respo
    
    * Terra Test
    
    * a
    
    * conf
    
    * Product Main

---
+++ src/Components/MainPageComponents/ProductMain.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+import test from "../../assets/4ikonanew2.webp";
+
+const ProductMain: React.FC = () => {
+  return (
+    <section className="pt-20">
+      <div className="text-3xl font-semibold ">
+        <h2>Zapoznaj się z naszymi produktamia</h2>
+      </div>
+      <div className="w-full flex flex-wrap pl-2 pr-2 justify-center mt-10">
+        <div className="product-container w-2/12 p-6 flex items-center ml-2 mr-2 mb-3">
+          <img src={test} alt="loading err" />
+          <div className="text-lg ml-3">Kołki szybkiego montażu</div>
+        </div>
+        <div className="product-container w-2/12 p-6 flex items-center ml-2 mr-2 mb-3">
+          <img src={test} alt="loading err" />
+          <div className="text-lg ml-3">Kołki szybkiego montażu</div>
+        </div>
+        <div className="product-container w-2/12 p-6 flex items-center ml-2 mr-2 mb-3">
+          <img src={test} alt="loading err" />
+          <div className="text-lg ml-3">Kołki szybkiego montażu</div>
+        </div>
+        <div className="product-container w-2/12 p-6 flex items-center ml-2 mr-2 mb-3">
+          <img src={test} alt="loading err" />
+          <div className="text-lg ml-3">Kołki szybkiego montażu</div>
+        </div>
+        <div className="product-container w-2/12 p-6 flex items-center ml-2 mr-2 mb-3">
+          <img src={test} alt="loading err" />
+          <div className="text-lg ml-3">Kołki szybkiego montażu</div>
+        </div>
+        <div className="product-container w-2/12 p-6 flex items-center ml-2 mr-2 mb-3">
+          <img src={test} alt="loading err" />
+          <div className="text-lg ml-3">Kołki szybkiego montażu</div>
+        </div>
+        <div className="product-container w-2/12 p-6 flex items-center ml-2 mr-2 mb-3">
+          <img src={test} alt="loading err" />
+          <div className="text-lg ml-3">Kołki szybkiego montażu</div>
+        </div>
+        <div className="product-container w-2/12 p-6 flex items-center ml-2 mr-2 mb-3">
+          <img src={test} alt="loading err" />
+          <div className="text-lg ml-3">Kołki szybkiego montażu</div>
+        </div>
+        <div className="product-container w-2/12 p-6 flex items-center ml-2 mr-2 mb-3">
+          <img src={test} alt="loading err" />
+          <div className="text-lg ml-3">Kołki szybkiego montażu</div>
+        </div>
+        <div className="product-container w-2/12 p-6 flex items-center ml-2 mr-2 mb-3">
+          <img src={test} alt="loading err" />
+          <div className="text-lg ml-3">Kołki szybkiego montażu</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProductMain;
--- src/Pages/Mian.tsx
+++ src/Pages/Mian.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import MainSlider from "../Components/MainPageComponents/MainSlider";
+import ProductMain from "../Components/MainPageComponents/ProductMain";
 
 const Main: React.FC = () => {
-  return <MainSlider />;
+  return (
+    <>
+      <MainSlider />
+      <ProductMain />
+    </>
+  );
 };
 
 export default Main;
