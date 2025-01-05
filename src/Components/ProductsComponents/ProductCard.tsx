7fc3cb277d757147f98f5e95a2e549106aad0b7d
Samessprog <100785560+Samessprog@users.noreply.github.com>
2025-01-05 21:03:11 +0100

    LGZ chanegs

--- src/Components/ProductsComponents/ProductCard.tsx
+++ src/Components/ProductsComponents/ProductCard.tsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { PropagateLoader } from "react-spinners";
+import imgTest from "../../../../AdamSklep/KGD/Untitled-1.jpg";
 
-const ProductCard = ({ img }) => {
-  return <div>he</div>;
+const ProductCard = ({
+  img,
+  short_Description,
+  product_type,
+  product_brand,
+  product_name,
+}) => {
+  return (
+    <div className="product-card">
+      <figure>
+        <Suspense fallback={<div>{PropagateLoader}</div>}>
+          <img src={imgTest} alt="loading err" />
+        </Suspense>
+        <figcaption className="text-justify product-desc pl-1 pr-1 pt-2">
+          <em className="font-semibold">{product_name}</em> -{" "}
+          {short_Description}
+        </figcaption>
+      </figure>
+      <button className="mt-2 product-button pt-2 pb-2 w-full text-white">
+        Szczegóły
+      </button>
+    </div>
+  );
 };
 export default ProductCard;
