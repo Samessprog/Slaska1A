b71d90daaa78debd855b8618b528669c87e87127
Samessprog <100785560+Samessprog@users.noreply.github.com>
2025-01-06 19:15:03 +0100

    better img loader

--- src/Components/ProductsComponents/ProductCard.tsx
+++ src/Components/ProductsComponents/ProductCard.tsx
@@ -1,6 +1,5 @@
 import React, { Suspense } from "react";
 import { PropagateLoader } from "react-spinners";
-import imgTest from "../../../../AdamSklep/KGD/Untitled-1.jpg";
 
 const ProductCard = ({
   img,
@@ -10,17 +9,23 @@ const ProductCard = ({
   product_name,
 }) => {
   return (
-    <div className="product-card">
-      <figure>
-        <Suspense fallback={<div>{PropagateLoader}</div>}>
-          <img src={imgTest} alt="loading err" />
-        </Suspense>
-        <figcaption className="text-justify product-desc pl-1 pr-1 pt-2">
-          <em className="font-semibold">{product_name}</em> -{" "}
-          {short_Description}
-        </figcaption>
-      </figure>
-      <button className="mt-2 product-button pt-2 pb-2 w-full text-white">
+    <div className="shadow-card">
+      <div className="product-card">
+        <figure>
+          <Suspense
+            fallback={
+              <div className="product-img-loader">{<PropagateLoader />}</div>
+            }
+          >
+            <img src={img} alt="loading err" />
+          </Suspense>
+          <figcaption className="text-justify product-desc pl-1 pr-1 pt-2 mb-2">
+            <em className="font-semibold">{product_name}</em> -{" "}
+            {short_Description}
+          </figcaption>
+        </figure>
+      </div>
+      <button className=" product-button pt-2 pb-2 w-full text-white">
         Szczegóły
       </button>
     </div>
